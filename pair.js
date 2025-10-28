088b3b649b25cd45b412218f1e7eea0cb5a3185e
ZeroBugZone417 <dinethsudarshana417@gmail.com>
2025-10-28 07:25:35 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const fs = require('fs-extra');
 const path = require('path');
@@ -48,7 +49,6 @@ const config = {
     CHANNEL_LINK: 'https://whatsapp.com/channel/0029VbAUZUeJENy0fOUS5E3J'
     
 }
-};
 
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN
@@ -788,83 +788,6 @@ case 'song': {
 //================================ tiktok ==================================
 
 
-// TIK TOK COMMAND
-case 'ttdl': {
-  const link = args.join(" ");
-  
-  try {
-    if (!link) {
-      await socket.sendMessage(from, {text: "Please enter valid tik tok video link !!"});
-    }
-    
-    const ttTharuzzApi = await fetch(https://tharuzz-ofc-apis.vercel.app/api/download/ttdl?url=${link});
-    const ttResponseTharuzz = await ttTharuzzApi.json();
-    
-    if (!ttResponseTharuzz?.result) {
-      await socket.sendMessage(from, {text: "No result found :("})
-    }
-    
-    const {title, duration, play_count, digg_count} = ttResponseTharuzz.result;
-    
-    const ttCap = `*📥 \NEROX TIK TOK DOWNLOADER\*\n\n` +
-      *┏━━━━━━━━━━━━━━━*\n +
-      `*┃ 📌 \ᴛɪᴛʟᴇ:\* ${title || "N/A"}\n` +
-      `*┃ ⏰ \ᴅᴜʀᴀᴛɪᴏɴ:\ ${duration || "N/A"}*\n` +
-      `*┃ 👀 \ᴠɪᴇᴡꜱ:\ ${play_count || "N/A"}*\n` +
-      `*┃ 🤍 \ʟɪᴋᴇꜱ:\ ${digg_count || "N/A"}*\n` +
-      `*┃ 📎 \ᴜʀʟ:\ ${link}*\n` +
-      *┗━━━━━━━━━━━━━━━━━━*\n +
-      *\𝚂𝙴𝙻𝙴𝙲𝚃 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 𝚃𝚈𝙿𝙴 ⬇\*\n\n + config.BOT_FOOTER;
-
-      const buttonPanel = [{
-      buttonId: "action",
-      buttonText: { displayText: "🔢 ꜱᴇʟᴇᴄᴛ ᴠɪᴅᴇᴏ ᴛʏᴘᴇ" },
-      type: 4,
-      nativeFlowInfo: {
-        name: "single_select",
-        paramsJson: JSON.stringify({
-          title: "🔢 ꜱᴇʟᴇᴄᴛ ᴠɪᴅᴇᴏ ᴛʏᴘᴇ",
-          sections: [{
-            title: "TIK TOK DOWNLOADER 📥",
-            highlight_label: "",
-            rows: [
-              {
-                title: "🎟 ᴡɪᴛʜᴏᴜᴛ ᴡᴀᴛᴇʀᴍᴀʀᴋ",
-                description: "Download video without watermark.",
-                id: ${config.PREFIX}ttdltharuzz NO_WM ${link}
-              },
-              {
-                title: "🎫 ᴡɪᴛʜ ᴡᴀᴛᴇʀᴍᴀʀᴋ",
-                description: "Download video with watermark.",
-                id: ${config.PREFIX}ttdltharuzz WM ${link}
-              },
-              {
-                title: "🎶 ᴀᴜᴅɪᴏ ꜰɪʟᴇ",
-                description: "Download video audio.",
-                id: ${config.PREFIX}ttdltharuzz AUDIO ${link}
-              }
-            ]
-          }]
-        })
-      }
-    }];
-    
-    await socket.sendMessage(from, {
-      image: { url: ttResponseTharuzz.result.cover || config.IMAGE_PATH },
-      caption: ttCap,
-      buttons: buttonPanel,
-      headerType: 1,
-      viewOnce: true
-    }, { quoted: msg });
-      
-    
-  } catch (e) {
-    console.log(e);
-    await socket.sendMessage(from, {text: "❌ Error: " + e});
-  }
-  break;
-};
-
 case 'ttdltharuzz': {
   await socket.sendMessage(sender, { react: { text: '📥', key: msg.key } });
 
@@ -872,7 +795,7 @@ case 'ttdltharuzz': {
   const mediaType = q.split(" ")[0];
   const mediaLink = q.split(" ")[1];
   try {
-    const ttApi = await fetch(https://tharuzz-ofc-apis.vercel.app/api/download/ttdl?url=${mediaLink});
+    const ttApi = await fetch (https://tharuzz-ofc-apis.vercel.app/api/download/ttdl?url=${mediaLink});
     const response = await ttApi.json();
     if (!response.result.hd || !response.result.sd) {
       await socket.sendMessage(from, { text: "No download link found !!" }, { quoted: msg });
